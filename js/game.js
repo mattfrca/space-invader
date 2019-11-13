70d10022fde98ffeeaba6ae0cdb1035be579e85f
mattfrca <matthieu.schille@hotmail.fr>
2019-11-13 09:49:13 +0100

    petit update

--- js/game.js
+++ js/game.js
@@ -7,8 +7,20 @@ const game = {
     init: function(){
         console.log("Jeu correctement lancé");
         // C'est ici qu'on commence à coder notre jeu.
-
+        document.addEventListener('keyup', game.moveTheShip);
         god.createElement("ship", 450, 50);
     },
-
+    moveTheShip: function(e) {
+        let unitWrapper = document.getElementsByClassName('unitWrapper')[0];
+        let posUnitWrapper = unitWrapper.style.cssText;
+        console.log(posUnitWrapper);
+        if(e.keyCode === 37) {
+            unitWrapper.style.right -= 5;
+            console.log('gauche pressée');
+        }
+        if(e.keyCode === 39) {
+            unitWrapper.style.right += 5;
+            console.log('droite pressée');
+        }
+    }
 };
