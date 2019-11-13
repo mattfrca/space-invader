61eee123dfef674041a7c82083511f73d5a95211
mattfrca <matthieu.schille@hotmail.fr>
2019-11-13 11:19:46 +0100

    update ship done

--- js/game.js
+++ js/game.js
@@ -1,36 +1,47 @@
 const game = {
 
 
-    ennemy: [],
-    ship: [],
+    ennemy: [], //ennemy[0][0] => posX de l'ennemi 1 
+
+
+    ship: [], //ship[0][0] posX   ship[0][1] posY
 
     gauche: false,
     droite: false,
+    vitesse: 10,
 
     init: function(){
         // C'est ici qu'on commence à coder notre jeu.
         document.addEventListener('keyup', game.handleKeyUp);
         document.addEventListener('keydown', game.handleKeydown);
-        god.createElement("ship", 450, 150);
+        game.ship = god.createElement("ship", 450, 500);
         //Créer ici les éléments
+        ennemy.createLigne(0, 'myth', 7);
+        ennemy.createLigne(1, 'squid', 7);
+        ennemy.createLigne(2, 'crab', 7);
+        ennemy.createLigne(3, 'space', 7);
         setInterval(game.handleTime, 20);
     },
 
     handleTime:function(){
         //Coeur de notre jeux
         game.moveTheShip();
-
-    },
-
-    moveTheShip: function() {
-        let unitWrapper = document.getElementsByClassName('ship');
         
-        if(game.droite){
-            console.log("vaisseau droite");
+    },
+    
+    moveTheShip: function() {
+        let unitWrapper = document.getElementsByClassName('ship')[0];
+        if(game.droite && game.ship[0] >= 10){
+            app.panel.removeChild(unitWrapper);
+            game.ship[0] -= this.vitesse;
+            god.createElement("ship", game.ship[0], 500);
         }
-        if(game.gauche){
-            console.log("vaisseau gauche");
+        if(game.gauche && game.ship[0] <= 830){
+            app.panel.removeChild(unitWrapper);
+            game.ship[0] += this.vitesse;
+            god.createElement("ship", game.ship[0], 500);
         }
+        
     },
 
     handleKeydown: function(evt){
